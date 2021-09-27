783a9b3929fc7a85875ccb2124b291c3f93005c4
Masha Evtihevich <evtihevich1996@gmail.com>
2021-09-27 20:52:12 +0300

    feat: add footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+export const Footer = () => (
+  <footer className="footer">
+    <p className="footer__text">2021, Animal Welfare</p>
+  </footer>
+);
