dce4e4277975e0047b389cef1e8b8b949eb967d0
Masha Evtihevich <evtihevich1996@gmail.com>
2021-09-27 21:00:24 +0300

    fix: request from get

--- src/pages/catalog/dog/CatalogDog.js
+++ src/pages/catalog/dog/CatalogDog.js
@@ -1,41 +1,52 @@
+import axios from "axios";
 import React from "react";
-import data from "./data.json";
+import { Footer } from "../../../components/footer/Footer";
 import "../index.css";
 
-export const CatalogDog = () => {
-  return (
-    <div className="content">
-      <div className="content__img content__img-dog"></div>
-      <h3 className="content__title">Help them find their way home!</h3>
-      <p className="content__text">
-        On this page of our website you can get acquainted with our wards: dogs
-        and puppies. Our wards are healthy, adult cats and cats are
-        neutered/sterilized, accustomed to a tray with wood filler. Animals are
-        transferred to a new family for free as a gift. By clicking on the photo
-        of the animal that interests you, you will see more photos, learn about
-        the nature of the animal.
-      </p>
-      <main className="content__main">
-        {data.map((postDetail, index) => {
-          return (
-            <div key={index} className="content-catalog">
-              <img
-                className="content-catalog__image"
-                src={postDetail.background}
-                alt="dog"
-              />
-              <p className="content-catalog__title">{postDetail.name} -</p>
-              <p className="content-catalog__text-age">{postDetail.age}</p>
-              <p className="content-catalog__text-gender">
-                {postDetail.gender}
-              </p>
-            </div>
-          );
-        })}
-      </main>
-      <footer className="footer">
-        <p className="footer__text">2021, Animal Welfare</p>
-      </footer>
-    </div>
-  );
-};
+class CatalogDog extends React.Component {
+  state = {
+    pets: [],
+  };
+
+  componentDidMount() {
+    axios.get(`http://localhost:3001/dogs`).then((res) => {
+      const pets = res.data;
+      this.setState({ pets });
+    });
+  }
+
+  render() {
+    return (
+      <div className="content">
+        <div className="content__img content__img-dog"></div>
+        <h3 className="content__title">Help them find their way home!</h3>
+        <p className="content__text">
+          On this page of our website you can get acquainted with our wards:
+          cats and kittens. Our wards are healthy, adult cats and cats are
+          neutered/sterilized, accustomed to a tray with wood filler. Animals
+          are transferred to a new family for free as a gift. By clicking on the
+          photo of the animal that interests you, you will see more photos,
+          learn about the nature of the animal.
+        </p>
+        <main className="content__main">
+          {this.state.pets.map((pet, index) => {
+            return (
+              <div key={index} className="content-catalog">
+                <img
+                  className="content-catalog__image"
+                  src={pet.background}
+                  alt="cat"
+                />
+                <p className="content-catalog__title">{pet.name} -</p>
+                <p className="content-catalog__text-age">{pet.age}</p>
+                <p className="content-catalog__text-gender">{pet.gender}</p>
+              </div>
+            );
+          })}
+        </main>
+        <Footer />
+      </div>
+    );
+  }
+}
+export default CatalogDog;
